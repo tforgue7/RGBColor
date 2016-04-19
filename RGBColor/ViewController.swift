3bec0b840f61c97e9cfa6c345259bc0bc1fb1020
tyler forgue <tjfnwtechcsi@tylers-macbook-pro.local>
2016-04-19 13:39:38 -0600

    Initial Commit

---
+++ RGBColor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGBColor
+//
+//  Created by Tyler Forgue on 4/19/16.
+//  Copyright © 2016 Tyler Forgue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
